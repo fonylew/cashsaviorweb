cf0c7cbe64fd4162eddec3611be42c5fff199e4e
Fony Lewprasert <kamolphan@hotmail.com>
2015-05-17 11:32:49 +0700

    First Commit

---
+++ dashboard.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html" />
+	<meta charset="utf-8"/>
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1,user-scalable=no" name="viewport"/>
+	<link rel="stylesheet" href="css/bootstrap.css"/>
+	<link rel="stylesheet" href="css/drawer.css.map"/>
+	<link rel="stylesheet" href="css/drawer.min.css"/>
+	<link href="css/favicon.ico" rel="icon"/>
+	<link href="css/apple-touch-icon.png" rel="apple-touch-icon"/>
+	<script src="js/jquery-1.11.2.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/iScroll/5.1.1/iscroll-min.js"></script>
+    <script src="//cdn.rawgit.com/ungki/bootstrap.dropdown/3.3.1/dropdown.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/jquery.drawer.js"></script>
+    <script src="js/jquery.drawer.min.js"></script>
+    <title>Cash Savior</title>
+</head>
+<body class="drawer drawer-left drawer-responsive drawer-navbar drawer-fixed">
+
+  <header role="banner">
+  <div class="drawer-header">
+    <button type="button" class="drawer-toggle drawer-hamburger">
+      <span class="sr-only">toggle navigation</span>
+      <span class="drawer-hamburger-icon"></span>
+    </button>
+  </div>
+
+  <div class="drawer-main drawer-default">
+    <nav class="drawer-nav" role="navigation">
+      <div class="drawer-brand"><a href="./">CashSavior</a></div>
+
+      <ul class="drawer-menu">
+        <li class="drawer-menu-item"><a href="index.php">info</a></li>
+      </ul>
+
+      <div class="drawer-footer"><span></span></div>
+    </nav>
+  </div>
+  </header>
+
+  <div class="drawer-overlay">
+    <h3>DASHBOARD</h3>
+    <p>This will be Dashboard in near future.</p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+  </div>
+
+</body>
+</html>
