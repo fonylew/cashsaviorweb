0208bf9945784c2215c383fa3b8577c9203df2a2
Pikarix <nabiki.unun@gmail.com>
2015-05-21 04:36:48 +0700

    Index has carousalllllllllll

--- index.php
+++ index.php
@@ -13,9 +13,96 @@
     <script src="js/bootstrap.js"></script>
     <title>Cash Savior</title>
     <meta charset="UTF-8">
+
+    <style>
+      .carousel-inner > .item > img,
+      .carousel-inner > .item > a > img {
+          width: 70%;
+          margin: auto;
+      }
+      #fleft{
+        float: left;
+        width: 26%;
+        height: 200px;
+        border: 3px solid black;
+        margin: 30px solid lightgrey;
+        padding: 30px;
+      }
+      #fleft2{
+        float: left;
+        width: 4%;
+        height: 200px;
+      }
+      #fleft3{
+        float: left;
+        width: 5%;
+        height: 200px;
+      }
+    </style>
 </head>
 
 <body>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+            <img src="pic/Investment.jpg" alt="Chania">
+            <div class="carousel-caption">
+              <h3>Investment</h3>
+              <p>Give an improvement to your income.</p>
+            </div>
+          </div>
+
+          <div class="item">
+            <img src="pic/saving.jpg" alt="Chania">
+            <div class="carousel-caption">
+              <h3>Saving</h3>
+              <p>Always prepare for emergency pay.</p>
+            </div>
+          </div>
+
+          <div class="item">
+            <img src="pic/entertainment.jpg" alt="Flower">
+            <div class="carousel-caption">
+              <h3>Entertainment</h3>
+              <p>Life is about happiness.</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+      <div class = "main">
+        <div class  = "container tim-container">
+          <div class="row">
+            <div class="tim-title">
+              <h1>Cash Savior</h1>
+            </div>
+            <div class="form-group">
+          
+             <input type="text" value="" placeholder="Username" class="form-control" />
+             
+             <input type="text" value="" placeholder="Password" class="form-control" />
+
+             <button type="submit" class="btn btn-block btn-lg btn-info">submit</button> 
+            </div>
+          </div>
+          
     <script>
         // This is called with the results from from FB.getLoginStatus().
         function statusChangeCallback(response) {
@@ -109,9 +196,15 @@
 
     <a href="#" onclick="checkLoginState();"><img class="displayed" src="img/facebook-login.png" border="0" alt="Log in with Facebook">
     </a>
-
     <div id="status">
     </div>
+    <div class = "row ">
+            <div class="content">
+              <h1>content1</h1>
+            </div>
+          </div>
+        </div>
+      </div>
 
 </body>
 
